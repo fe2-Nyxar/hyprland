759e884e3b8f339e1a6b253d4c6b5634941698a0
fe2-Nyxar <nyxar49@gmail.com>
2024-09-20 09:29:11 +0100

    first commit! :D

---
+++ start.sh
@@ -0,0 +1,6 @@
+swww init &
+swww img ~/Pictures/wallpaper-pfp/theme/wallhaven.png &
+#ags --config /etc/greetd/greeter.js: hyprctl dispatch exit &
+eww -c ~/.config/eww/ open bar &
+dunst &
+wl-paste --type text --watch cliphist store & wl-paste --type image --watch cliphist store
